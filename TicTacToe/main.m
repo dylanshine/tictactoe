02349053c0467b5bfc8635ef4ce84765b1d193f6
Dylan Shine <dylanshinenyc@gmail.com>
2015-04-17 14:45:32 -0400

    Initial Commit

---
+++ TicTacToe/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TicTacToe
+//
+//  Created by Dylan Shine on 4/17/15.
+//  Copyright (c) 2015 Dylan Shine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
